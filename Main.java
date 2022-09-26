8725cda7b33ec6f1a372440251ecf635aee5019e
unknown <castro_angel@ciencias.unam.mx>
2022-09-26 15:56:28 -0500

    Se termino el juego de las canicas. Nota: revisar si hay otra forma de actualizar las variables de las canicas

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main{
     String contrasena;
     
     //Ademas de contruir un objeto de clase canicas, tambien se inicialia el juego con 10 canicas cada quien.
-    Canicas ca = new Canicas();
+    Canicas canicas1 = new Canicas();
 
     //Variables que nos ayudara a verificar usuarios
     String accesoUsuario1 = "Miguel";
@@ -38,47 +38,107 @@ public class Main{
     if(accesoUsuario2.equals(usuario) && accesoContrasena2.equals(contrasena)){
 
         System.out.println("Bienvenido a Calculadora");
+        System.out.println("\n");
         
     }else if(accesoUsuario1.equals(usuario) && accesoContrasena1.equals(contrasena)){
 
-            System.out.println("Opciones disponibles para ejecutar. " +
-                                "\n" + "Para juego de las Canicas oprima 1" + 
-                                "\n" + "Para Calculadora oprima 2");
+            System.out.println("Opciones disponibles. " +
+                                "\n" + "1: Juego de las Canicas." + 
+                                "\n" + "2: Calculadora.");
             
             int opcion = sc.nextInt();
             
             if(opcion == 1){
 
                 System.out.print("Bienvenido al juego de canicas!");
-                System.out.println("Tu contricante de hoy sera la PC");
+                System.out.println("\n");
+                System.out.println("Tu contricante de hoy sera: ***CPU***");
 
                 //Imprime que cada uno de los jugadores tiene 10 canicas cada quien.
-                System.out.println("El juego inicia con: " + "\n" + ca);
+                System.out.println("El juego inicia con: " + "\n" + canicas1);
+                System.out.print("\n");
 
-                int canicasPC;
-                int canicasUs;
+                int canicasPC = 0;
+                int canicasUs = 0;
 
-                while(canicasPC < 20 || canicasUs < 20){
+                boolean centinela = true;
 
-                    canicasPC = this.getCanicasCPU();
-                    canicasUs = this.getCanicasUsuario(); 
+                while(centinela){   
+                    
+                    canicasPC = canicas1.getCanicasCPU();
+                    canicasUs = canicas1.getCanicasUsuario();
 
-                    System.out.println("Momento de apostar");
+                    System.out.println("---Momento de apostar---");
+                    System.out.print("\n");
+
                     System.out.print("Ingresa tu apuesta: ");
                     int apuestaDelUsuario = sc.nextInt();
                     System.out.print("\n");
-                    System.out.print("Tu prediccion sera par o impar(0 para par y 1 para impar)");
+
+                    System.out.print("Tu prediccion sera par o impar(0 para par y 1 para impar): ");
                     int decisionUsuario = sc.nextInt();
+                    System.out.print("\n");
                     
                     //Apuesta de la pc.
                     int apuestaDeLaPC = aleatorios.nextInt(canicasPC) + 1;
-
-                    ca.apuestaUsuario(apuestaDelUsuario, decisionUsuario, apuestaDeLaPC);
-
-                    System.out.println(ca);
                     
+                    //El usuario apuesta y se valida su apuesta si no esta postando mas de lo que tiene
+                    if(apuestaDelUsuario <= canicasUs){
+                    canicas1.apuestaUsuario(apuestaDelUsuario, decisionUsuario, apuestaDeLaPC);
 
+                    System.out.println("Asi quedaron: " + "\n" + canicas1);
+
+                    }else{
+                        System.out.println("Estas apostando mas de lo que tienes");
+                    }
+
+                    canicasPC = canicas1.getCanicasCPU();
+                    canicasUs = canicas1.getCanicasUsuario();
+
+                    if(canicasUs==20 && canicasPC ==0){
+
+                        centinela = false;
+                        System.out.print("Haz ganado!!!!");
+                        break;
+
+                    }else if(canicasPC==20 && canicasUs==0){
+                        centinela = false;
+                        System.out.print("Haz perdido :((");
+                        break;
+                    }
+
+                    System.out.println("---Momento de apostar de la CPU---");
+
+                    int apuestaDeLaPC2 = aleatorios.nextInt(canicasPC) + 1;
+                    int decisionCPU = aleatorios.nextInt(1);
+
+                    System.out.print("Introduce una nueva apuesta");
+                    int apuestaDelUsuario2 = sc.nextInt();
+                    System.out.print("\n");
+
+                    if(apuestaDelUsuario2 <= canicasUs){
+
+                        canicas1.apuestaCPU(apuestaDeLaPC2, decisionCPU, apuestaDelUsuario2);
+                        System.out.println("Asi quedaron: " + "\n" + canicas1);
+
+                    }else {
+                        System.out.println("Estas apostando mas de lo que tienes");
+                    }
+
+                    canicasPC = canicas1.getCanicasCPU();
+                    canicasUs = canicas1.getCanicasUsuario();
                     
+                    if(canicasUs==20 && canicasPC ==0){
+
+                        centinela = false;
+                        System.out.print("Haz ganado!!!!");
+                        break;
+
+                    }else if(canicasPC==20 && canicasUs==0){
+                        centinela = false;
+                        System.out.print("Toco perder :((");
+                        break;
+                    }
 
                 }
 
